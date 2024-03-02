2c1212be9a8eadd24a967acd217bbea9f629e77c
gokulraj18 <rgokul527@gmail.com>
2024-03-02 21:37:48 +0530

    feat:updated

---
+++ college/pattern7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class pattern1{
+    public static void main(String args[]){
+        int i,j;
+        for(i=5;i>=1;i--){
+            for(j=5;j>=i;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+// 5
+// 54
+// 543
+// 5432
+// 54321
